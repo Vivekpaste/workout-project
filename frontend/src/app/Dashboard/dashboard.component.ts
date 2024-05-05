656d6a2a52e424656a2e2b9adcf80275737f2f17
Vivek Paste <vivek.eknath@gmail.com>
2024-05-05 15:35:38 +0530

    ts
    mafe changes in ts for Dashboard,login and also css part in legs

--- frontend/src/app/Dashboard/dashboard.component.ts
+++ frontend/src/app/Dashboard/dashboard.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { WorkoutService,WorkoutType } from '../workout.service';
+import { HttpClient } from '@angular/common/http';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-dashboard',
@@ -7,12 +11,38 @@ import { WorkoutService,WorkoutType } from '../workout.service';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
-  Name='';
+  name='';
   workoutType!: WorkoutType;
-
-  constructor(private workoutService: WorkoutService) { }
+  userInfo:any;
+  greeting:string | undefined;
+  weight:any;
+  height:any;
+  types:  "rest" | undefined;
+  constructor(private workoutService: WorkoutService, private http:HttpClient,private router:Router, private formBuilder:FormBuilder) { }
 
   ngOnInit(): void {
+    const currentTime=new Date().getHours();
     this.workoutType = this.workoutService.getWorkoutType();
+  
+    this.getinfo();
+    if (currentTime >= 0 && currentTime < 12) {
+      this.greeting = 'Good morning';
+    } else if (currentTime >= 12 && currentTime < 18) {
+      this.greeting = 'Good afternoon';
+    } else {
+      this.greeting = 'Good evening';
+    }
+
+  
   }
+
+  getinfo(): void {
+    this.http.get<any>('/api/getinfo/')
+      .subscribe(data => {
+        this.name = data.name;
+        this.weight=data.weight;
+        this.height=data.height;
+      });
+  }
+
 }
